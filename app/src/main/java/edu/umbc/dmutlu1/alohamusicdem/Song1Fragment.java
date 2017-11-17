4ae24e9a3329e3ff4fec6cf2b40232ced2bdfbd7
dmutlu <devin.mutlu+github@gmail.com>
2017-11-17 02:50:57 -0500

    Added working fragments.

---
+++ app/src/main/java/edu/umbc/dmutlu1/alohamusicdem/Song1Fragment.java
@@ -0,0 +1,39 @@
+package edu.umbc.dmutlu1.alohamusicdem;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Song1Fragment extends Fragment
+{
+
+    public Song1Fragment()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_song1, container, false);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     */
+    public interface OnFragmentInteractionListener
+    {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
